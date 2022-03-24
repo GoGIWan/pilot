1b858fa3d5ff76565ff50ef46b31c4607b6d553d
GOGIWAN <daliman@naver.com>
2022-03-24 10:22:24 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 function App() {
+  // 서버와 통신하는 메소드
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    // 서버 url
+    fetch("/api/hello")
+      .then((response) => response.text())
+      .then((message) => {
+        setMessage(message);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">{message}</h1>
       </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
     </div>
   );
 }
-
 export default App;
